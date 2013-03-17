d787c4f07912ce688051dbef0ebf8a582e53b9df
tcme <hi@this-connect.me>
2013-03-17 19:07:57 +0100

    paadds parsing options with https://github.com/substack/node-optimist (wip)

--- index.js
+++ index.js
@@ -1,44 +1,15 @@
 var app = require('./lib/app');
 
-app.options = {
-	'dirname': __dirname
-	, 'server': {
-		'host': 'localhost'
-		, 'port': 8004
-	}
-	, 'io' : {
-		'transports': ['websocket']
-		, 'flash policy server': false
-		, 'log level': 1
-		, 'resource': '/io'
-		//, 'browser client': false
-		//, 'browser client cache': true
-		//, 'browser client minification': true
-		//, 'browser client gzip': true
-	}
-	, 'ui': 'lite'
-	, 'pd': {
-		'read': 8005
-		, 'write': 8006
-		, 'flags': [
-			'-noprefs'
-			//, '-nogui'
-			, '-stderr'
-			, '-path'
-			, './pd'
-			, '-path'
-			, './pd/blib'
-			, '-open'
-			, './pd/manager.pd'
-		]
-	}
-};
-
 require('./lib/monitor');
 require('./lib/ui');
 require('./lib/desktop');
 require('./lib/planet');
 require('./lib/pd');
 require('./lib/server');
+require('./lib/option');
+
+app.on('option setup', function(){
+	app.emit('option set', 'dirname', __dirname);
+});
 
 app.emit('setup');
