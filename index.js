69d972f70d069f9bcdcf79f630812732bfbb631a
thisconnect <hi@this-connect.me>
2014-02-07 03:51:03 +0100

    config fix

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var app = require('./lib/app');
 
 require('./lib/log');
-require('./lib/socket');
-require('./lib/pd');
 require('./lib/engine');
+// require('./lib/pd');
 require('./lib/server');
+require('./lib/socket');
 require('./lib/interface');
 require('./lib/desktop');
 
